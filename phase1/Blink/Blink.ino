ff2a5422951e11b7471650791e9d683bcd552ce2
XavierDionLacasse <xavidl@icloud.com>
2026-05-15 19:06:12 -0400

    feat: blink amélioré avec boucle for 3 clignotements

--- phase1/Blink/Blink.ino
+++ phase1/Blink/Blink.ino
@@ -30,8 +30,11 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // change state of the LED by setting the pin to the LOW voltage level
-  delay(1000);                      // wait for a second
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+  }
+  delay(1000);
 }
